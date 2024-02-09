d597a832183132ecc7aa484e98f42dbcb9d6232f
K <Redknclarke@gmail.com>
2024-02-08 19:22:59 -0500

    added shuffling and switched from array to vector

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,8 @@ use opengl_graphics::{GlGraphics, OpenGL};
 use piston::event_loop::*;
 use piston::input::*;
 use piston::window::WindowSettings;
-
+use rand::seq::SliceRandom;
+use rand::thread_rng;
 const WIDTH: i16 = 500;
 const HEIGHT: i16 = 500;
 const ARRAY_SIZE: usize = 100;
@@ -16,35 +17,36 @@ const BAR_WIDTH: i16 = WIDTH / ARRAY_SIZE as i16;
 
 struct Game {
     gl: GlGraphics,
-    array: [i8; ARRAY_SIZE],
+    array: Vec<i8>,
 }
 
 impl Game {
     fn new(gl: GlGraphics) -> Game {
-        let mut arr = [0; ARRAY_SIZE];
-        for num in 1..ARRAY_SIZE {
-            arr[num - 1] = num as i8;
-        }
+        let arr: Vec<i8> = (0..ARRAY_SIZE as i8).collect();
         Game { gl, array: arr }
     }
 
     fn render(&mut self, arg: &RenderArgs) {
         self.gl.draw(arg.viewport(), |_c, gl| {
             graphics::clear([0.0, 0.0, 0.0, 1.0], gl);
-            for n in self.array {
+            for (i, n) in self.array.iter().enumerate() {
                 graphics::rectangle(
                     [1.0, 1.0, 1.0, 1.0],
                     [
-                        (n as i16 * BAR_WIDTH) as f64,
+                        (i as i16 * BAR_WIDTH) as f64,
                         0.0,
                         BAR_WIDTH as f64,
-                        n as f64 * (HEIGHT as f64 / ARRAY_SIZE as f64),
+                        *n as f64 * (HEIGHT as f64 / ARRAY_SIZE as f64),
                     ],
                     _c.transform,
                     gl,
                 );
             }
         });
+    }
+
+    fn shuffle(&mut self) {
+        self.array.shuffle(&mut thread_rng());
     }
 }
 
@@ -65,6 +67,9 @@ fn main() {
     while let Some(e) = events.next(&mut window) {
         if let Some(r) = e.render_args() {
             game.render(&r);
+        }
+        if let Some(b) = e.button_args() {
+            game.shuffle();
         }
     }
 }
